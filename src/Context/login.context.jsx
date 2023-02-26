b3e3139854fcb173662100a2295e920824a2ecab
Raitis <96236875+RaitisVilums@users.noreply.github.com>
2023-02-26 21:39:33 +0200

    Add files via upload

--- src/Context/login.context.jsx
+++ src/Context/login.context.jsx
@@ -1,11 +1,13 @@
 import React, { createContext, useState } from "react";
+import Notification from "../Components/Features/Popup/Notification/notification.popup";
 
-const LoginContext = createContext();
+export const LoginContext = createContext();
 
-const LoginContextProvider = (props) => {
+export const LoginContextProvider = (props) => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [message, setMessage] = useState("");
 
   const handleLogin = (email, password) => {
     console.log(email, password);
@@ -13,25 +15,33 @@ const LoginContextProvider = (props) => {
       console.log(`Login successful`);
       setIsLoggedIn(true);
       localStorage.setItem("isLoggedIn", true);
+      setMessage("Login successful!");
     } else {
       console.log(`Wrong email or password`);
     }
   };
 
+  const handleClearMessage = () => {
+    setMessage("");
+  };
+
   return (
-    <LoginContext.Provider
-      value={{
-        isLoggedIn,
-        email,
-        setEmail,
-        password,
-        setPassword,
-        handleLogin,
-      }}
-    >
-      {props.children}
-    </LoginContext.Provider>
+    <>
+      {message && (
+        <Notification message={message} onClearMessage={handleClearMessage} />
+      )}
+      <LoginContext.Provider
+        value={{
+          isLoggedIn,
+          email,
+          setEmail,
+          password,
+          setPassword,
+          handleLogin,
+        }}
+      >
+        {props.children}
+      </LoginContext.Provider>
+    </>
   );
 };
-
-export { LoginContext, LoginContextProvider };
