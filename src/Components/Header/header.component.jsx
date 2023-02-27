5e038030339aed2656388b27fe9e4e03ebc1c68d
Raitis <96236875+RaitisVilums@users.noreply.github.com>
2023-02-27 13:29:29 +0200

    Add files via upload

--- src/Components/Header/header.component.jsx
+++ src/Components/Header/header.component.jsx
@@ -1,8 +1,21 @@
+import React, { useState, useEffect } from "react";
 import DesktopHeader from "./HeaderComponents/desktop-header.component";
 import PhoneHeader from "./HeaderComponents/phone-header.component";
 
 const Header = () => {
-  return <PhoneHeader />;
+  const [isDesktop, setIsDesktop] = useState(window.innerWidth > 600);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsDesktop(window.innerWidth > 699);
+    };
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, [isDesktop]);
+
+  return isDesktop ? <DesktopHeader /> : <PhoneHeader />;
 };
 
 export default Header;
